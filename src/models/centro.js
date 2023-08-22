c565338bdf297e87cfc143d37e26a9bd4fc9f2da
Pedro R. Spidalieri <pedro.spidalieri@gmail.com>
2023-08-22 20:48:46 -0300

    Initial commit

---
+++ src/models/centro.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const esquemaCentro = mongoose.Schema({
+    
+})
+
+module.exports = mongoose.model('Centro', esquemaCentro)
---
+++ src/models/tipo_centro.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const esquemaTipoCentro = mongoose.Schema({
+    
+})
+
+module.exports = mongoose.model('TipoCentro', esquemaTipoCentro)
